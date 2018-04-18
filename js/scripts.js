6cf89724559a7128f2c332b6465280905b903583
Petr Petukhov <kotov0991@gmail.com>
2018-04-19 02:03:36 +0300

    form logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 
 $(function() {
 
-	// tarrif plan active card
+	// Фиксирование состояние рекомендованной карточки тарифного плана
 	$(".tarrifs-item").hover( function(){
 		$(".tarrifs-item").eq(0).removeClass('recommend');	
 	});
+
 	$(".tarrifs-item").mouseleave(function(){
 		$(".tarrifs-item").eq(0).addClass('recommend');
 	});
@@ -22,7 +23,6 @@ $(function() {
 
 	$('.my-tooltip').tooltip();
 
-	// footer links sliding
 	function adjustCollapseView(){
 		var desktopView = $(document).width();
 		if(desktopView >= "768"){
@@ -42,17 +42,222 @@ $(function() {
 	});
 
 
-	// password comparison
-	$(".error").hide();
-	$('.restrict-str').keypress(function(key) { if(key.charCode < 48 || key.charCode > 57) return false; });
-	$("#password-confirm").change(function() {
-		$(".error").hide();
-		var valueX = $("#password").val();
-		var valueY = $("#password-confirm").val();
-		if (valueX != valueY) {
-			$(".error").show();
+	
+
+	// FORM VALIDATION
+	$('.card-firstName, .card-lastName').keypress(function(key) { ;
+		if((key.charCode >= 33 && key.charCode <= 64) || (key.charCode <= 91 && key.charCode <= 96) || (key.charCode >= 123 && key.charCode <= 127)) return false;
+	});
+
+	$('.card-number').mask("0000 0000 0000 0000");
+	$('.card-date').mask("00 / AZ", {
+		translation: {
+			'A': {
+				pattern: /[0-1]/, optional: true	
+			},
+			'Z': {
+				pattern: /[0-2]/, optional: true
+			}
 		}
 	});
+	$('.card-cvv').mask("000");
+	$('.card-postal').mask("000");
+	$('.card-coupon').mask("0000 0000 0000 0000");
+
+	$('.tarrifs-item').click(function() {
+		$('#login-collapse').collapse();
+		var top = $('#login-collapse').offset().top;
+		$('body,html').animate({scrollTop: top-100}, 1000);
+	});
+
+	$("#login-form").validate({
+			rules: {
+				email: {
+					required: true,
+					email: true
+				},
+				password: {
+					required: true
+				},
+				password_confirm: {
+					required: true,
+					equalTo: "#password"
+				}
+			},
+			messages: {
+				email: "Please enter a valid email address",
+				password: {
+					required: "Please provide a password",
+					minlength: "Your password must be at least 5 characters long"
+				},
+				password_confirm: {
+					required: "Please provide a password",
+					minlength: "Your password must be at least 5 characters long",
+					equalTo: "Please enter the same password as above"
+				}
+			}
+		});
+
+
+	$('.form-text-btn').click(function(event) {
+		event.preventDefault();
+		if($("#login-form").valid()) {
+			$('#step-accordion').collapse();
+		}
+	});
+
+
+
+	$(".payment-form1").validate({
+			rules: {
+				inputFirstName1: "required",
+				inputLastName1: "required",
+				inputCardNumber1: {
+						required: true,
+						minlength: 16
+					},
+				inputExpirationDate1: {
+						required: true,
+						minlength: 7
+					},
+				inputCvv1: {
+					required: true,
+					minlength: 3
+				},
+				inputPostalCode1: {
+					required: true,
+					minlength: 3
+				},
+				inputCoupon1: {
+					minlength: 16
+				}	
+			},
+			messages: {
+				inputFirstName1: "Please enter your firstname",
+				inputLastName1: "Please enter your lastname",
+				inputCardNumber1: {
+					required: "Please provide a card number",
+					minlength: "The number must contain at least 16 numbers"
+				},
+				inputExpirationDate1: {
+					required: "Please provide an expiration date",
+					minlength: "The number must contain at least 4 numbers"
+				},
+				inputCvv1: {
+					required: "Please provide a cvv number",
+					minlength: "The number must contain at least 3 numbers"
+				},
+				inputPostalCode1: {
+					required: "Please provide a postal code",
+					minlength: "The number must contain at least 3 numbers"
+				},
+				inputCoupon1: {
+					minlength: "The number must contain at least 16 numbers"
+				},
+			}
+		});
+
+	$(".payment-form2").validate({
+			rules: {
+				inputFirstName2: "required",
+				inputLastName2: "required",
+				inputCardNumber2: {
+						required: true,
+						minlength: 16
+					},
+				inputExpirationDate2: {
+						required: true,
+						minlength: 7
+					},
+				inputCvv2: {
+					required: true,
+					minlength: 3
+				},
+				inputPostalCode2: {
+					required: true,
+					minlength: 3
+				},
+				inputCoupon2: {
+					minlength: 16
+				}	
+			},
+			messages: {
+				inputFirstName2: "Please enter your firstname",
+				inputLastName2: "Please enter your lastname",
+				inputCardNumber2: {
+					required: "Please provide a card number",
+					minlength: "The number must contain at least 16 numbers"
+				},
+				inputExpirationDate2: {
+					required: "Please provide an expiration date",
+					minlength: "The number must contain at least 4 numbers"
+				},
+				inputCvv2: {
+					required: "Please provide a cvv number",
+					minlength: "The number must contain at least 3 numbers"
+				},
+				inputPostalCode2: {
+					required: "Please provide a postal code",
+					minlength: "The number must contain at least 3 numbers"
+				},
+				inputCoupon2: {
+					minlength: "The number must contain at least 16 numbers"
+				},
+			}
+		});
+
+	$(".payment-form3").validate({
+			rules: {
+				inputFirstName3: "required",
+				inputLastName3: "required",
+				inputCardNumber3: {
+						required: true,
+						minlength: 16
+					},
+				inputExpirationDate3: {
+						required: true,
+						minlength: 7
+					},
+				inputCvv3: {
+					required: true,
+					minlength: 3
+				},
+				inputPostalCode3: {
+					required: true,
+					minlength: 3
+				},
+				inputCoupon3: {
+					minlength: 16
+				}	
+			},
+			messages: {
+				inputFirstName3: "Please enter your firstname",
+				inputLastName3: "Please enter your lastname",
+				inputCardNumber3: {
+					required: "Please provide a card number",
+					minlength: "The number must contain at least 16 numbers"
+				},
+				inputExpirationDate3: {
+					required: "Please provide an expiration date",
+					minlength: "The number must contain at least 4 numbers"
+				},
+				inputCvv3: {
+					required: "Please provide a cvv number",
+					minlength: "The number must contain at least 3 numbers"
+				},
+				inputPostalCode3: {
+					required: "Please provide a postal code",
+					minlength: "The number must contain at least 3 numbers"
+				},
+				inputCoupon3: {
+					minlength: "The number must contain at least 16 numbers"
+				},
+			}
+		});
+
+
+
+
 
 });
 
